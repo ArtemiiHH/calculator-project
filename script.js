f41d8143be6ecd2a2c47d11698b408d03e027e47
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-08-31 18:18:06 +0300

    Fix replacing 0 with a button pressed

--- script.js
+++ script.js
@@ -6,29 +6,28 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button-box button');
 
-let num1 = '';
-let num2 = '';
-let operator = '';
-
 // Get all buttons (NodeList)
 buttons.forEach(btn => {
     // Button actions
     btn.addEventListener('click', () => {
         const rawValue = btn.textContent;
         const value = Number(rawValue);
-        
-        if (value >= 0 && value <= 9) display.textContent = `${value}`;
 
+        if (rawValue >= 0 && rawValue <= 9 && display.textContent === '0') {
+            display.textContent = `${rawValue}`;
+        } else {
+            display.textContent += `${rawValue}`
+        };
         if (rawValue === '=') display.textContent = `${operate()}`;
         if (rawValue === 'C') display.textContent = '0';
     });
 });
 
 const operate = (num1, num2, operator) => {
-    if (operator === '+') add(num1, num2);
-    if (operator === '-') subtract(num1, num2);
-    if (operator === 'x') multiply(num1, num2);
-    if (operator === '/') divide(num1, num2);
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === 'x') return multiply(num1, num2);
+    if (operator === '/') return divide(num1, num2);
 };
 
 // Operation functions
