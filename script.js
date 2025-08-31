aca0d3c384e230f8b4239c4201ea81f22430001d
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-08-31 14:42:22 +0300

    Remove h2 from dispaly + add basic js structure

---
+++ script.js
@@ -0,0 +1,20 @@
+// Calculator Project
+
+
+
+// DOM elements
+const display = document.querySelector('#display');
+
+
+const num1 = 0;
+const num2 = 0;
+const operator = '';
+
+// Operation functions
+const add = () => {};
+
+const subtract = () => {};
+
+const multiply = () => {};
+
+const divide = () => {};
