737860346acd5d952a023670981bbffd2c827e56
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-08-31 15:27:30 +0300

    Get buttons NodeList + add event listener to buttons

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 
 // DOM elements
-const display = document.querySelector('#display');
-
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button-box button');
 
 const num1 = 0;
 const num2 = 0;
@@ -18,3 +18,11 @@ const subtract = () => {};
 const multiply = () => {};
 
 const divide = () => {};
+
+// Get all buttons (NodeList)
+buttons.forEach(btn => {
+    // Button actions
+    btn.addEventListener('click', () => {
+        
+    });
+});
